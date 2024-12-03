e250450067584bac63b3b704b9a0ca96e1de9396
Graham Edgecombe <gpe@grahamedgecombe.com>
2024-12-03 08:35:17 +0000

    Add day 3 part 2 solution

--- src/main/kotlin/com/grahamedgecombe/advent2024/day3/Day3.kt
+++ src/main/kotlin/com/grahamedgecombe/advent2024/day3/Day3.kt
@@ -1,22 +1,49 @@
 package com.grahamedgecombe.advent2024.day3
 
 import com.grahamedgecombe.advent2024.Puzzle
+import com.grahamedgecombe.advent2024.day3.Day3.Insn
 
-object Day3 : Puzzle<List<Pair<Int, Int>>>(3) {
-    private val REGEX = Regex("mul\\(([0-9]{1,3}),([0-9]{1,3})\\)")
+object Day3 : Puzzle<List<Insn>>(3) {
+    sealed interface Insn {
+        object Enable : Insn
+        object Disable : Insn
+        data class Mul(val a: Int, val b: Int) : Insn
+    }
 
-    override fun parse(input: Sequence<String>): List<Pair<Int, Int>> {
+    private val REGEX = Regex("(do|don't|mul)\\((?:([0-9]{1,3}),([0-9]{1,3}))?\\)")
+
+    override fun parse(input: Sequence<String>): List<Insn> {
         return buildList {
             for (line in input) {
                 for (match in REGEX.findAll(line)) {
-                    val (a, b) = match.destructured
-                    add(Pair(a.toInt(), b.toInt()))
+                    val (type, a, b) = match.destructured
+
+                    when (type) {
+                        "do" -> add(Insn.Enable)
+                        "don't" -> add(Insn.Disable)
+                        "mul" -> add(Insn.Mul(a.toInt(), b.toInt()))
+                    }
                 }
             }
         }
     }
 
-    override fun solvePart1(input: List<Pair<Int, Int>>): Int {
-        return input.sumOf { (a, b) -> a * b }
+    override fun solvePart1(input: List<Insn>): Int {
+        return input.filterIsInstance<Insn.Mul>().sumOf { (a, b) -> a * b }
+    }
+
+    override fun solvePart2(input: List<Insn>): Int {
+        var sum = 0
+        var enabled = true
+
+        for (insn in input) {
+            when {
+                insn == Insn.Enable -> enabled = true
+                insn == Insn.Disable -> enabled = false
+                insn is Insn.Mul && enabled -> sum += insn.a * insn.b
+            }
+        }
+
+        return sum
     }
 }
--- src/test/kotlin/com/grahamedgecombe/advent2024/day3/Day3Test.kt
+++ src/test/kotlin/com/grahamedgecombe/advent2024/day3/Day3Test.kt
@@ -6,13 +6,22 @@ import org.junit.jupiter.api.Test
 class Day3Test {
     @Test
     fun testPart1() {
-        assertEquals(161, Day3.solvePart1(TEST_INPUT))
+        assertEquals(161, Day3.solvePart1(TEST_INPUT_1))
         assertEquals(179571322, Day3.solvePart1(PROD_INPUT))
     }
 
+    @Test
+    fun testPart2() {
+        assertEquals(48, Day3.solvePart2(TEST_INPUT_2))
+        assertEquals(103811193, Day3.solvePart2(PROD_INPUT))
+    }
+
     private companion object {
-        private val TEST_INPUT = Day3.parse("""
+        private val TEST_INPUT_1 = Day3.parse("""
             xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))
+        """.trimIndent())
+        private val TEST_INPUT_2 = Day3.parse("""
+            xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))
         """.trimIndent())
         private val PROD_INPUT = Day3.parse()
     }
