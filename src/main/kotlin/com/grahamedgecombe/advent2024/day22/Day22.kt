f4d16bbb76665751fab2e8173834eb6d532955a0
Graham Edgecombe <gpe@grahamedgecombe.com>
2024-12-22 09:30:42 +0000

    Add day 22 part 2 solution

--- src/main/kotlin/com/grahamedgecombe/advent2024/day22/Day22.kt
+++ src/main/kotlin/com/grahamedgecombe/advent2024/day22/Day22.kt
@@ -3,22 +3,68 @@ package com.grahamedgecombe.advent2024.day22
 import com.grahamedgecombe.advent2024.Puzzle
 
 object Day22 : Puzzle<List<Int>>(22) {
+    private const val ROUNDS = 2000
+
     override fun parse(input: Sequence<String>): List<Int> {
         return input.map(String::toInt).toList()
     }
 
     override fun solvePart1(input: List<Int>): Long {
         return input.sumOf { secret ->
-            next(secret, 2000).toLong()
+            next(secret, ROUNDS).toLong()
         }
+    }
+
+    private data class ChangeSeq(val a: Int, val b: Int, val c: Int, val d: Int)
+
+    override fun solvePart2(input: List<Int>): Int {
+        val bananas = mutableMapOf<ChangeSeq, Int>()
+
+        val changes = ArrayDeque<Int>()
+        val seen = mutableSetOf<ChangeSeq>()
+
+        for (secret in input) {
+            var secret = secret
+            var previousPrice = secret % 10
+
+            changes.clear()
+            seen.clear()
+
+            for (i in 0 until ROUNDS) {
+                secret = next(secret)
+                val price = secret % 10
+
+                changes += price - previousPrice
+                previousPrice = price
+
+                if (changes.size > 4) {
+                    changes.removeFirst()
+                }
+
+                if (changes.size != 4) {
+                    continue
+                }
+
+                val seq = ChangeSeq(changes[0], changes[1], changes[2], changes[3])
+                if (seen.add(seq)) {
+                    bananas[seq] = price + bananas.getOrDefault(seq, 0)
+                }
+            }
+        }
+
+        return bananas.values.max()
+    }
+
+    private fun next(secret: Int): Int {
+        var secret = (secret xor (secret shl 6)) and 0xFFFFFF
+        secret = (secret xor (secret shr 5)) and 0xFFFFFF
+        return (secret xor (secret shl 11)) and 0xFFFFFF
     }
 
     private fun next(secret: Int, rounds: Int): Int {
         var secret = secret
         repeat(rounds) {
-            secret = (secret xor (secret shl 6)) and 0xFFFFFF
-            secret = (secret xor (secret shr 5)) and 0xFFFFFF
-            secret = (secret xor (secret shl 11)) and 0xFFFFFF
+            secret = next(secret)
         }
         return secret
     }
--- src/test/kotlin/com/grahamedgecombe/advent2024/day22/Day22Test.kt
+++ src/test/kotlin/com/grahamedgecombe/advent2024/day22/Day22Test.kt
@@ -6,15 +6,27 @@ import org.junit.jupiter.api.Test
 class Day22Test {
     @Test
     fun testPart1() {
-        assertEquals(37327623, Day22.solvePart1(TEST_INPUT))
+        assertEquals(37327623, Day22.solvePart1(TEST_INPUT_1))
         assertEquals(13764677935, Day22.solvePart1(PROD_INPUT))
     }
 
+    @Test
+    fun testPart2() {
+        assertEquals(23, Day22.solvePart2(TEST_INPUT_2))
+        assertEquals(1619, Day22.solvePart2(PROD_INPUT))
+    }
+
     private companion object {
-        private val TEST_INPUT = Day22.parse("""
+        private val TEST_INPUT_1 = Day22.parse("""
             1
             10
             100
+            2024
+        """.trimIndent())
+        private val TEST_INPUT_2 = Day22.parse("""
+            1
+            2
+            3
             2024
         """.trimIndent())
         private val PROD_INPUT = Day22.parse()
